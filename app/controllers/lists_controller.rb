457f55ca93a345faa3fbb150c390cb7bc7c6fada
Mathis <kmzmathis@gmail.com>
2026-02-28 17:57:12 +0100

    rails watch list finished

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,13 +17,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(list_params)
-    # @list.photo_list.attach(io: @list.photo_list, filename: "list_#{@list.name}_photo", content_type: "image/avif")
     if @list.save
       redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
-    # raise
   end
 
   private
